8af45b1c96c55f0e13bdc307056116868d842b8b
jm <jianbiaomei@zju.edu.cn>
2023-05-24 18:12:49 +0800

    disengled

--- projects/mmdet3d_plugin/sgn/dense_heads/__init__.py
+++ projects/mmdet3d_plugin/sgn/dense_heads/__init__.py
@@ -2,3 +2,4 @@ from .sgn_head import SGNHead
 from .sgn_head_cross import SGNHeadCross
 from .sgn_head_lss import SGNHeadLSS
 from .sgn_head_one_stage import SGNHeadOne
+from .sgn_head_disengled import SGNHeadDS
